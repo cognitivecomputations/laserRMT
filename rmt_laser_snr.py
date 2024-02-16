b5af2b99c5efd7901508784dce83d50b4f7f3ed9
Lucas Hänke de Cansino <lhc@next-boss.eu>
2024-02-16 10:40:10 +0100

    refactored get_test_tokens to only get and tokinze test / validation split

--- rmt_laser_snr.py
+++ rmt_laser_snr.py
@@ -114,7 +114,7 @@ class ModelModifier:
         total_samples = 0
 
         for dataset in datasets:
-            input_tok = gptq_data_utils.get_test_tokens(dataset, seed=0, seqlen=seqlen, model=model_str)
+            input_tok = gptq_data_utils.get_test_tokens(dataset, n_samples=256, seed=0, seqlen=seqlen, model=model_str)
             nsamples = input_tok.numel() // seqlen
             input_tok = input_tok[0, :(seqlen * nsamples)].view(nsamples, seqlen)
             total_samples += nsamples
